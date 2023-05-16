a0fb62b7a681126963b0b7f0b8b038be9630f14b
Sambert Rodrigues <sambert@smartshore.nl>
2023-05-16 15:32:55 +0530

    composables

---
+++ pages/hello/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container py-3 mx-auto main">
+        <h1 class="text-teal-500">Hello Nuxt 3</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+    const { sayHello } = useUtils();
+    sayHello();
+</script>
+
+<style scoped>
+
+</style>
